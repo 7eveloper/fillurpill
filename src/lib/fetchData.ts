ef1edb267978e561ac3148fccc1c2bdb47419be6
lauichan <te06230623@gmail.com>
2024-03-21 12:31:43 +0900

    fix: 안쓰는 함수 제거

--- src/lib/fetchData.ts
+++ src/lib/fetchData.ts
@@ -1,16 +1,5 @@
-import { createClient } from "@supabase/supabase-js";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { supabase } from "./supabase";
-
-export const getData = async (
-  pageParam: number,
-  keyword: string | null = null
-) => {
-  if (keyword == null) {
-    return await fetchData(pageParam);
-  }
-  return await searchData(pageParam, keyword);
-};
 
 export const fetchData = async (pageParam: number) => {
   const { data, error } = await supabase
@@ -18,7 +7,7 @@ export const fetchData = async (pageParam: number) => {
     .select("*")
     .range((pageParam - 1) * 10, (pageParam - 1) * 10 + 9);
   if (error) throw error;
-  console.log("검색어없음", data);
+  console.log(data);
   return data;
 };
 
@@ -29,14 +18,14 @@ export const searchData = async (pageParam: number, keyword: string) => {
     .textSearch("function", keyword)
     .range((pageParam - 1) * 10, (pageParam - 1) * 10 + 9);
   if (error) throw error;
-  console.log(keyword, data);
+  console.log(data);
   return data;
 };
 
-export const useQueryProduct = (keyword: string) => {
+export const useQueryProduct = () => {
   return useInfiniteQuery({
     queryKey: ["product"],
-    queryFn: ({ pageParam }) => getData(pageParam, keyword),
+    queryFn: ({ pageParam }) => fetchData(pageParam),
     initialPageParam: 1,
     getNextPageParam: (lastPage, allPages) => {
       if (lastPage.length < 10) return null;
@@ -48,17 +37,19 @@ export const useQueryProduct = (keyword: string) => {
   });
 };
 
-// export const useSearchProduct = (keyword: string) => {
-//   return useInfiniteQuery({
-//     queryKey: ["search"],
-//     queryFn: ({ pageParam }) => searchData(pageParam, keyword),
-//     initialPageParam: 1,
-//     getNextPageParam: (lastPage, allPages) => {
-//       if (lastPage.length < 10) return null;
-//       return allPages.length + 1;
-//     },
-//     select: (data) => {
-//       return data.pages.flat();
-//     },
-//   });
-// };
+export const useSearchProduct = (keyword: string) => {
+  return useInfiniteQuery({
+    queryKey: ["search"],
+    queryFn: ({ pageParam }) => searchData(pageParam, keyword),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPages) => {
+      if (lastPage.length < 10) return null;
+      return allPages.length + 1;
+    },
+    select: (data) => {
+      return data.pages.flat();
+    },
+  });
+};
+
+export type SearchData = ReturnType<typeof useSearchProduct>["data"];
