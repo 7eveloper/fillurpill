8430dcb6a9196a47a2625e0a2498a328ba888637
lauichan <te06230623@gmail.com>
2024-03-21 15:54:23 +0900

    update: 검색기능 포함된 제품페이지

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,26 +1,27 @@
 "use client";
+
 import ProductList from "@/components/product/ProductList";
-import { useInfiniteScroll } from "@/hooks/useInfiniteScroll";
-import { useQueryProduct } from "@/lib/fetchData";
+import SearchBar from "@/components/product/SearchBar";
+import { useQueryProduct } from "@/hooks/useQueryProduct";
+import { useSearchParams } from "next/navigation";
+import { useState } from "react";
 
-const ProductPage = () => {
-  const {
-    data = [],
-    hasNextPage,
-    isFetchingNextPage,
-    fetchNextPage,
-  } = useQueryProduct();
+const ProductListPage = () => {
+  const params = useSearchParams();
+  const query = params.get("q") ?? "";
+  const [searchType, setSearchType] = useState(params.get("type") ?? "");
 
-  const pageEnd = useInfiniteScroll(hasNextPage, fetchNextPage);
+  const { data, isFetchingNextPage, pageEnd } = useQueryProduct(query);
 
   return (
-    <>
+    <section>
       <h1>전체 제품</h1>
+      <SearchBar />
       <ProductList data={data} />
       <div ref={pageEnd}>더보기</div>
       {isFetchingNextPage ? <div>loading</div> : null}
-    </>
+    </section>
   );
 };
 
-export default ProductPage;
+export default ProductListPage;
