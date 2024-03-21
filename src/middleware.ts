98a3db4148a0de1d2bf84437e7b126da8d8ca2ff
injeong Hwang <twilight9758@gmail.com>
2024-03-21 14:57:32 +0900

    fix: 불필요한 주석 삭제

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,15 +4,11 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  // console.log(req.url);
-  // console.log(req.nextUrl);
-  // console.log(req.nextUrl.origin);
   const res = NextResponse.next();
 
   const supabase = createMiddlewareClient({ req, res });
 
   const { data } = await supabase.auth.getSession();
-  // console.log(data.session);
   const { data: userResults, error } = await supabase
     .from("survey")
     .select("*")
@@ -24,11 +20,7 @@ export async function middleware(req: NextRequest) {
     !(req.nextUrl.pathname === "/")
   ) {
     return NextResponse.redirect(new URL("/login", req.url));
-  }
-  // if (!data.session && !(req.nextUrl.pathname === "/")) {
-  //   return NextResponse.redirect(new URL("/login", req.url));
-  // }
-  else if (data.session && req.nextUrl.pathname.startsWith("/login")) {
+  } else if (data.session && req.nextUrl.pathname.startsWith("/login")) {
     return NextResponse.redirect(new URL("/", req.nextUrl.origin));
   } else if (
     data.session &&
@@ -39,7 +31,6 @@ export async function middleware(req: NextRequest) {
   } else {
     return res;
   }
-  // return res;
 }
 
 export const config = {
