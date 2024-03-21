ac3f31beaff377c75aa0e0f18e6b4c54316424c8
lauichan <te06230623@gmail.com>
2024-03-21 15:53:09 +0900

    add: productList 로직 커스텀훅으로 분리

---
+++ src/hooks/useQueryProduct.ts
@@ -0,0 +1,29 @@
+import { fetchData } from "@/lib/fetchData";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { useInfiniteScroll } from "./useInfiniteScroll";
+
+export const useQueryProduct = (query: string) => {
+  const { data, hasNextPage, isFetchingNextPage, fetchNextPage, refetch } =
+    useInfiniteQuery({
+      queryKey: ["search"],
+      queryFn: ({ pageParam }) => fetchData(pageParam, query),
+      initialPageParam: 1,
+      getNextPageParam: (lastPage, allPages) => {
+        if (lastPage.length < 10) return null;
+        return allPages.length + 1;
+      },
+      select: (data) => {
+        return data.pages.flat();
+      },
+    });
+
+  useEffect(() => {
+    refetch();
+  }, [query]);
+
+  const pageEnd = useInfiniteScroll(hasNextPage, fetchNextPage);
+  return { data, isFetchingNextPage, pageEnd };
+};
+
+export type SearchData = ReturnType<typeof useQueryProduct>["data"];
