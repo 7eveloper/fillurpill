e0cddac624d69c4ee75080aca2de65e87d213b9d
injeong Hwang <twilight9758@gmail.com>
2024-03-22 10:53:24 +0900

    fix: 메인에 Alert 추가(임시)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import GameApp from "@/components/customUi/GameApp";
 import SurveyDrawerDemo from "./survey/survey-action/Survey";
+import { Alert } from "@/components/ui/alert";
+import { AlertDemo } from "@/components/customUi/AlertDemo";
 
 export default async function Home() {
   return (
