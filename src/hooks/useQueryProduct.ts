243492bcca520c6a82600d605d4d02bdbf9a540b
lauichan <te06230623@gmail.com>
2024-03-22 23:25:12 +0900

    update: 페이지당 제품개수 상수로 설정

--- src/hooks/useQueryProduct.ts
+++ src/hooks/useQueryProduct.ts
@@ -1,18 +1,17 @@
 "use client";
-import { fetchData } from "@/lib/fetchData";
+import { PAGINATE, fetchData } from "@/lib/fetchData";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { useInfiniteScroll } from "./useInfiniteScroll";
 
 export const useQueryProduct = (query: string, searchType: string) => {
-  console.log(searchType);
   const { data, hasNextPage, isFetchingNextPage, fetchNextPage, refetch } =
     useInfiniteQuery({
       queryKey: ["search"],
       queryFn: ({ pageParam }) => fetchData(pageParam, query, searchType),
       initialPageParam: 1,
       getNextPageParam: (lastPage, allPages) => {
-        if (lastPage.length < 10) return null;
+        if (lastPage.length < PAGINATE) return null;
         return allPages.length + 1;
       },
       select: (data) => {
