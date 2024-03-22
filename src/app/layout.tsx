d9d217581643f687cba85ef633ed271c7456956c
lauichan <te06230623@gmail.com>
2024-03-23 01:31:50 +0900

    fix: suppressHydrationWarning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default async function RootLayout({
 }>) {
   return (
     <QueryProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body
           className={cn(
             "min-h-screen bg-background font-sans antialiased",
