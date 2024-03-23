cca0d56d7971acbc75bedb26a20afd287e835aa8
lauichan <te06230623@gmail.com>
2024-03-23 19:34:33 +0900

    update: 로직 이동

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -4,35 +4,27 @@ import GoTopBtn from "@/components/product/GoTopBtn";
 import ProductList from "@/components/product/ProductList";
 import SearchBar from "@/components/product/SearchBar";
 import { useQueryProduct } from "@/hooks/useQueryProduct";
-import { useSearchParams } from "next/navigation";
-import { ChangeEvent, useState } from "react";
 
 const ProductListPage = () => {
-  const params = useSearchParams();
-  const query = params.get("q") ?? "";
-  const [searchType, setSearchType] = useState(
-    params.get("type") ?? "function"
-  );
-
-  const handleChangeType = (e: ChangeEvent<HTMLSelectElement>): void => {
-    setSearchType(e.target.value);
-  };
-
-  const { data, isFetchingNextPage, hasNextPage, pageEnd } = useQueryProduct(
-    query,
-    searchType
-  );
+  const {
+    data,
+    isFetching,
+    hasNextPage,
+    pageEnd,
+    searchType,
+    handleChangeType,
+  } = useQueryProduct();
 
   return (
     <>
-      <section className="max-w-[1360px] mx-auto">
+      <section className="max-w-[1360px] w-full mx-auto">
         <SearchBar
           searchType={searchType}
           handleChangeType={handleChangeType}
         />
         <ProductList data={data} />
         <div className="py-10 text-center" ref={pageEnd}>
-          {isFetchingNextPage ? <div>loading</div> : null}
+          {isFetching ? <div>loading</div> : null}
           {hasNextPage ? "더보기" : "다음 페이지가 없습니다"}
         </div>
       </section>
